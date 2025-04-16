13731a7cb56fb2b6967649343fd7106d8eff9ab7
Lucas Correa <lucas.nhl@gmail.com>
2025-04-16 00:17:33 -0300

    add transaction routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::group(['prefix' => 'transactions'], function () {
+        Route::post('/deposit', [TransactionController::class, 'deposit']);
+        Route::post('/withdraw', [TransactionController::class, 'withdraw']);
+        Route::post('/transfer', [TransactionController::class, 'transfer']);
+        Route::post('/revoke', [TransactionController::class, 'revokeTransaction']);
+    });
 });
